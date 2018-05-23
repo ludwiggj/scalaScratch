52a51c0931548763038bd04d88a3088f803b48c6
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2018-05-23 22:24:56 +0100

    Latest work, up to chapter 78

---
+++ src/main/scala/org/ludwiggj/fnprog/simplified/io/monad/IOTest1.scala
@@ -0,0 +1,32 @@
+package org.ludwiggj.fnprog.simplified.io.monad
+
+object IOTest1 {
+  def eagerIO(): Unit = {
+    import eager._
+
+    for {
+      _ <- putStrLn("First name?")
+      firstName <- getLine
+      _ <- putStrLn(s"Last name?")
+      lastName <- getLine
+      _ <- putStrLn(s"First: $firstName, Last: $lastName")
+    } yield ()
+  }
+
+  def lazyIO(): Unit = {
+    import lazee._
+
+    for {
+      _ <- putStrLn("1st name?")
+      firstName <- getLine
+      _ <- putStrLn(s"nth name?")
+      lastName <- getLine
+      _ <- putStrLn(s"First: $firstName, Last: $lastName")
+    } yield ()
+  }
+
+  def main(args: Array[String]): Unit = {
+    eagerIO()
+    lazyIO()  // Doesn't execute!
+  }
+}
